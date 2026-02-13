78e65985556ad5cf0951d7ef9148e99a6b4d3184
Muhammad Nouman <nomannisar769@gmail.com>
2026-02-13 13:07:50 +0500

    requirement update
    Signed-off-by: Muhammad Nouman <nomannisar769@gmail.com>

--- apps/cars/views.py
+++ apps/cars/views.py
@@ -7,6 +7,7 @@ from .serializers import CarSeriliazer
 from rest_framework.views import APIView
 from rest_framework.response import  Response
 from rest_framework.decorators import api_view
+from rest_framework import status
 
 '''------------- Manual Api View --------------'''
 def car_list(request):
@@ -33,6 +34,10 @@ def car_detail(request, pk):
 @api_view(['GET', 'POST'])
 def car_list_view(request):
     if request.method == 'GET':
+        try:
+            cars = Cars.objects.all()
+        except:
+            return Response({'error': 'No cars found'})
         car = Cars.objects.all()
         serializer = CarSeriliazer(car, many=True)
         return Response(serializer.data)
@@ -48,9 +53,24 @@ def car_list_view(request):
 
 '''------------- Signal object api ---------------'''
 
-@api_view()
+@api_view(['GET', 'POST','DELETE'])
 def car_detail_view(request, pk):
-    car = Cars.objects.get(pk=pk)
-    serializer = CarSeriliazer(car)
-    return Response(serializer.data)
-        
+    if request.method == 'GET':
+        car = Cars.objects.get(pk=pk)
+        serializer = CarSeriliazer(car)
+        return Response(serializer.data)
+    if request.method == 'PUT':
+        car = Cars.objects.get(pk=pk)
+        serializer = CarSeriliazer(car,data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+        if request.method == 'DELETE':
+            car = Cars.objects.get(pk=pk)
+            car.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        else:
+            return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
