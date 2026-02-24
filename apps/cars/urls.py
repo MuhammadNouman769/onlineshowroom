886a972421e7c4e165940e0ee97ed5451d64526f
Muhammad Nouman <nomannisar769@gmail.com>
2026-02-24 11:03:59 +0500

    update URL patterns for car and showroom detail views

--- apps/cars/urls.py
+++ apps/cars/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     #path('', views.car_list, name='car_list'),
     path('cars/', views.car_list_view,name='car_list'),
-    path('<int:pk>/', views.car_detail_view, name='car_detail'),
+    path('cars/<int:pk>/', views.car_detail_view, name='car_detail'),
     path('showroom/', views.Showroom_view.as_view(), name='showroom'),
-  #  path('<int:pk>/', views.car_detail, name='car_detail')
+    path('showroom/<int:pk>/', views.showroom_detail.as_view(), name='car_detail')
 ]
