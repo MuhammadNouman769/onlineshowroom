e4099f2c562d0a76fa4bbac85259607a8f758d08
Muhammad Nouman <nomannisar769@gmail.com>
2026-02-24 13:02:30 +0500

    add Swagger and Redoc documentation endpoints for API

--- core/urls.py
+++ core/urls.py
@@ -16,8 +16,33 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="ShowRoom API",
+        default_version='v1',
+        description="API docs for ShowRoom and Car endpoints",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('all/',include('apps.cars.urls'))
+    path('all/', include('apps.cars.urls')),
+
+    # Swagger UI
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+
+    # Redoc UI
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+
 ]
