efc8c91abb53f268346d8eaab1f07a486005dace
Muhammad Nouman <nomannisar769@gmail.com>
2026-02-24 13:02:17 +0500

    add drf_yasg and drf_spectacular to settings; configure REST framework schema class

--- core/settings.py
+++ core/settings.py
@@ -40,6 +40,9 @@ INSTALLED_APPS = [
     'apps.users',
     'apps.cars',
     'rest_framework',
+    'drf_yasg',
+    'drf_spectacular',
+
 ]
 
 MIDDLEWARE = [
@@ -124,3 +127,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
