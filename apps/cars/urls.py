eb3d7405074c0fa14f1d120a06a44a3b7f0e0aff
Muhammad Nouman <nomannisar769@gmail.com>
2026-02-24 13:30:33 +0500

    refactor urls.py; reorganize API routes for cars and showrooms, improving clarity and structure

--- apps/cars/urls.py
+++ apps/cars/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    #path('', views.car_list, name='car_list'),
-    path('cars/', views.car_list_view,name='car_list'),
+    # Cars
+    path('cars/', views.car_list_view, name='car_list'),
     path('cars/<int:pk>/', views.car_detail_view, name='car_detail'),
-    path('showroom/', views.ShowRoomListCreate.as_view(), name='showroom'),
-    path('showroom/<int:pk>/', views.ShowRoomDetail.as_view(), name='car_detail')
+
+    # ShowRooms
+    path('showrooms/', views.ShowRoomListCreate.as_view(), name='showroom_list'),
+    path('showrooms/<int:pk>/', views.ShowRoomDetail.as_view(), name='showroom_detail'),
 ]
