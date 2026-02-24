d2ba6706160297ef0d224b3c7e68deca04d57a59
Muhammad Nouman <nomannisar769@gmail.com>
2026-02-24 13:02:48 +0500

    update showroom API views to use class-based views for list and detail operations

--- apps/cars/urls.py
+++ apps/cars/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     #path('', views.car_list, name='car_list'),
     path('cars/', views.car_list_view,name='car_list'),
     path('cars/<int:pk>/', views.car_detail_view, name='car_detail'),
-    path('showroom/', views.Showroom_view.as_view(), name='showroom'),
-    path('showroom/<int:pk>/', views.showroom_detail.as_view(), name='car_detail')
+    path('showroom/', views.ShowRoomListCreate.as_view(), name='showroom'),
+    path('showroom/<int:pk>/', views.ShowRoomDetail.as_view(), name='car_detail')
 ]
